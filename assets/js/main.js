298e80d20822abd4316354137d66b821ddc77b54
Catalina Tapia <catalinantf@gmail.com>
2021-06-20 00:19:27 -0400

    commit 6 JS y toques finales

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 var nav = document.querySelector('nav');
 
 window.addEventListener('scroll', function () {
-   if (window.pageYOffset > 800) {
+   if (window.pageYOffset > 530) {
       nav.classList.add('bg-dark').add('slow');
    } else {
       nav.classList.remove('bg-dark');
